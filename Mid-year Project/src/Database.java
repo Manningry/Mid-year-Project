424c563490b5b5c1f60d95d3d01fd84e64a2e79c
ryan.manning <ryan.manning@MU-C3-02.mullen.pvt.k12.co.us>
2014-09-17 14:56:54 -0600

    Original push from school 9/17

---
+++ Mid-year Project/src/Database.java
@@ -0,0 +1,32 @@
+
+public class Database
+	{
+
+private double boxLength;
+private double boxHeight;
+private double boxWidth;
+private double boxWeight;
+private String boxDestination;
+	
+public Database(double bL, double bH,double bW, double bWe, String bD)
+	{
+	boxLength = bL;
+	boxHeight = bH;
+	boxWidth = bW;
+	boxWeight = bWe;
+	boxDestination = bD;
+	}
+
+public double getBoxLength()
+	{
+	return boxLength;
+	}
+
+public void setBoxLength(double boxLength)
+	{
+	this.boxLength = boxLength;
+	}
+
+
+
+	}
